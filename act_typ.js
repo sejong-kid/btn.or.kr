3137a096cf2cfa74709c7662889e9eb1f98cc8a4
qocksgh <123qocksgh@naver.com>
2022-12-09 17:46:25 +0900

    123

--- act_typ.js
+++ act_typ.js
@@ -1,33 +1,37 @@
+// 버튼 클릭=배경색 변경
 const body = document.querySelector('body'); // 태그를 호출한다.
 const btn = document.querySelector('button');
 const colors = ['lightgreen', 'lightblue', 'lightyellow']
-
 body.style.backgroundColor = 'white';
 btn.addEventListener('click', colorChange); // function 뒤에 ()가 없다.
-
 function colorChange() {
     const colorIdx = parseInt(Math.random()*colors.length); // 순서를 랜덤으로 만들기
     body.style.backgroundColor = colors[colorIdx]; // 랜덤으로 배열 안의 컬러를 선택한다.
 }
 
-
+// 키보드 이벤트
 window.addEventListener("keydown", e => {
   const key = document.getElementById(e.key);
   if (key) key.classList.add('pressed');
 });
-
-
 window.addEventListener("keyup", e => {
   const key = document.getElementById(e.key);
   if (key) key.classList.remove('pressed');
 });
 
-var change = document.getElementsByClassName("change")[0];
+// 버튼 클릭=한/영 전환
+var change = document.getElementById('change')[0];
+var text = document.querySelectorAll('button')[0];
+var eng = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+var kor = ["ㅂ","ㅈ","ㄷ","ㄱ","ㅅ","ㅛ","ㅕ","ㅑ","ㅐ","ㅔ","ㅁ","ㄴ","ㅇ","ㄹ","ㅎ","ㅗ","ㅓ","ㅏ","ㅣ","ㅋ","ㅌ","ㅊ","ㅍ","ㅠ","ㅜ","ㅡ"]
 
-var text = document.getElementsByClassName("Q")[0];
+// change.addEventListener("click",function(){
+//   eng.forEach(function(){
+//     eng.push(kor);
+//   });
+// })
 
-change.addEventListener ("click",function(){
-  if(text.textContent= "ㅂ");
-  
-});
-
+function change(eng) {
+  console.log(eng);
+}
+kor.forEach(kor);
